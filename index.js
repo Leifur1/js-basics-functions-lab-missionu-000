d98510aa7ce6834af7a2edd95d07dbd60f5c4c82
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-28 20:17:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,5 +40,7 @@ function calculatesFarePrice(start, destination) {
     return (distance - 400) * .02
   } else if ( distance > 2000 && distance < 2500) {
     return 25
+  } else {
+    return 'cannot travel that far'
   }
 }
