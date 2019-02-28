f31f4ee404325369edbd7db0bd78863d54303198
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-28 19:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Code your solution in this file!
 let dist
 function distanceFromHqInBlocks (distance) {
-  if (distance < 42) {
-    let dist = 42 - distance;
-  } else {
+  if (distance < 42)
     let dist = distance % 42
   }
   return dist
